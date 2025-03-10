3e566654e6a1fa308cf554828c2d621a9c317e53
Luuk Machielse <luuk.machielse@gmail.com>
2025-03-10 19:44:24 +0100

    Added Firefox configuration

--- home-manager/modules/firefox.nix
+++ home-manager/modules/firefox.nix
@@ -1,10 +1,3 @@
 {inputs, ...}: {
-  programs.firefox = {
-    enable = true;
-    profiles.luukm = {
-      extensions = with inputs.firefox-addon.packages."x86_64-linux"; [
-        ublock-origin
-      ];
-    };
-  };
+  programs.firefox.enable = true;
 }
