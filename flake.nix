448f7b10a517be9cceeb21ca53aa3e337f6198a5
Luuk Machielse <luuk.machielse@gmail.com>
2025-03-12 18:16:11 +0100

    Refactored flake.nix

--- flake.nix
+++ flake.nix
@@ -1,14 +1,10 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
 
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.pkgs.follows = "nixpkgs";
-    };
     hyprland.url = "github:hyprwm/Hyprland";
     catppuccin.url = "github:catppuccin/nix";
-
     firefox-addons = {
       url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
       inputs.nixpkgs.follows = "nixpkgs";
