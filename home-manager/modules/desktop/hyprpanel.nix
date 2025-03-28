f0400b623a8a8baf95474a23aa14f45b7d7107b8
luukm <luuk.machielse@gmail.com>
2025-03-28 16:22:02 +0100

    Changed HyprPanel config

--- home-manager/modules/desktop/hyprpanel.nix
+++ home-manager/modules/desktop/hyprpanel.nix
@@ -5,8 +5,21 @@
     enable = true;
     hyprland.enable = true;
 
-    settings.bar = {
-      launcher.icon = "";
+    settings = {
+      bar.launcher.icon = "";
+      menus.dashboard.shortcuts.left = {
+        shortcut1 = {
+          command = "firefox-developer-edition";
+          icon = "󰈹";
+          tooltip = "Firefox";
+        };
+        shortcut2 = {
+          command = "spotify";
+          icon = "";
+          tooltip = "Spotify";
+        };
+      };
+      menus.dashboard.directories.left.directory3.label = "󱧼";
     };
   };
 }
